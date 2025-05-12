d4efb9cd39707ca607d889a9166f17c2d3720865
maanizz <155484966+maanizz@users.noreply.github.com>
2025-05-12 09:24:14 +0500

    Add files via upload

---
+++ CrapsGame.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class CrapsGame {
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        int die1 = rollDie(rand);
+        int die2 = rollDie(rand);
+        int sum = die1 + die2;
+
+        System.out.println("You rolled " + die1 + " + " + die2 + " = " + sum);
+
+        if (sum == 7 || sum == 11) {
+            System.out.println("You win");
+        } else if (sum == 2 || sum == 3 || sum == 12) {
+            System.out.println("You lose");
+        } else {
+            int point = sum;
+            System.out.println("point is " + point);
+
+            while (true) {
+                die1 = rollDie(rand);
+                die2 = rollDie(rand);
+                sum = die1 + die2;
+                System.out.println("You rolled " + die1 + " + " + die2 + " = " + sum);
+
+                if (sum == point) {
+                    System.out.println("You win");
+                    break;
+                } else if (sum == 7) {
+                    System.out.println("You lose");
+                    break;
+                }
+            }
+        }
+    }
+
+    
+    public static int rollDie(Random rand) {
+        return rand.nextInt(6) + 1;
+    }
+}
